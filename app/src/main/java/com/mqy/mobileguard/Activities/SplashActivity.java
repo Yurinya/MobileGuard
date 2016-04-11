7a5bd67a41a282c1492a5a2e3e4c0930194adca6
yurinya <dlutmqy@163.com>
2016-04-11 17:03:26 +0800

    创建

---
+++ app/src/main/java/com/mqy/mobileguard/Activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.mqy.mobileguard.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.mqy.mobileguard.R;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+    }
+}
